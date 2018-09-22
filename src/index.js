d54ba47d01c76c8d6b582fe1073bf69162e81f03
baurine <2008.hbl@gmail.com>
2018-09-22 14:50:09 +0800

    4.2 - DragDropContext

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '@atlaskit/css-reset';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 import initialData from './initial-data';
 import Column from './Column';
@@ -8,15 +9,34 @@ import Column from './Column';
 class App extends React.Component {
   state = initialData
 
-  render() {
-    return this.state.columnOrder.map(columnId => {
-      const column = this.state.columns[columnId]
-      const tasks = column.taskIds.map(taskId => this.state.tasks[taskId])
+  onDragStart = result => {
+  }
 
-      return <Column key={column.id}
-                column={column}
-                tasks={tasks}/>
-    })
+  onDragUpdate = result => {
+  }
+
+  onDragEnd = result => {
+    // TODO: reorder tasks
+  }
+
+  render() {
+    return (
+      <DragDropContext
+        onDragStart={this.onDragStart}
+        onDragUpdate={this.onDragUpdate}
+        onDragEnd={this.onDragEnd}>
+        {
+          this.state.columnOrder.map(columnId => {
+            const column = this.state.columns[columnId]
+            const tasks = column.taskIds.map(taskId => this.state.tasks[taskId])
+
+            return <Column key={column.id}
+                      column={column}
+                      tasks={tasks}/>
+          })
+        }
+      </DragDropContext>
+    )
   }
 }
 
