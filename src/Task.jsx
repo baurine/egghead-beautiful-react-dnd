0701e7d52b30e6ebd8011d00b7004468dc75932b
baurine <2008.hbl@gmail.com>
2018-09-22 16:57:19 +0800

    10.1 - Disable draggable for some components

--- src/Task.jsx
+++ src/Task.jsx
@@ -7,21 +7,28 @@ const Container = styled.div`
   border-radius: 2px;
   padding: 8px;
   margin-bottom: 8px;
-  background-color: ${props => props.isDragging ? 'lightgreen' : 'white'};
+  background-color: ${props => 
+    props.isDragDisabled ? 'lightgrey' :
+    props.isDragging ? 'lightgreen' : 'white'};
   display: flex;
 `;
 
 export default class Task extends React.Component {
   render() {
+    const isDragDisabled = this.props.task.id === 'task-1'
     return (
-      <Draggable draggableId={this.props.task.id} index={this.props.index}>
-        { 
+      <Draggable
+        draggableId={this.props.task.id}
+        isDragDisabled={isDragDisabled}
+        index={this.props.index}>
+        {
           (provided, snapshot) =>
           <Container
             {...provided.draggableProps}
             {...provided.dragHandleProps}
             innerRef={provided.innerRef}
-            isDragging={snapshot.isDragging}>
+            isDragging={snapshot.isDragging}
+            isDragDisabled={isDragDisabled}>
             {this.props.task.content}
           </Container>
         }
