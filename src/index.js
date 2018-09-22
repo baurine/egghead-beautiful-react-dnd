5552ad55ac527a10ae6e135fff5920ae9b44551d
baurine <2008.hbl@gmail.com>
2018-09-22 12:54:31 +0800

    3.1 - Initial data and implement Column component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import initialData from './initial-data';
+import Column from './Column';
 
-const App = () => 'Hello React';
+class App extends React.Component {
+  state = initialData
+
+  render() {
+    return this.state.columnOrder.map(columnId => {
+      const column = this.state.columns[columnId]
+      const tasks = column.taskIds.map(taskId => this.state.tasks[taskId])
+
+      return <Column key={column.id}
+                column={column}
+                tasks={tasks}/>
+    })
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
