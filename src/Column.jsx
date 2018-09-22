2dd767239819a9d3aa8ce9aca681532ca3f8bc67
baurine <2008.hbl@gmail.com>
2018-09-22 13:49:23 +0800

    3.3 - Style the Column component

--- src/Column.jsx
+++ src/Column.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+`;
+const Title = styled.h3`
+  padding: 8px;
+`;
+const TaskList= styled.div`
+  padding: 8px;
+`;
 
 export default class Column extends React.Component {
   render() {
-    return this.props.column.title;
+    return (
+      <Container>
+        <Title>{this.props.column.title}</Title>
+        <TaskList>TaskList</TaskList>
+      </Container>
+    )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '@atlaskit/css-reset';
+
 import initialData from './initial-data';
 import Column from './Column';
 
